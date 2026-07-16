f1a00feb86347e1832ea95f341d1a89072d9339b
openCAE solutions <arthur@opencaesolutions.com>
2026-07-16 19:47:05 +0200

    Remove German comments
    German Comments removed

--- run_bash.bat
+++ run_bash.bat
@@ -7,14 +7,8 @@ set USERNAME=ofuser
 set USER=ofuser
 set HOME=/home/ofuser
 
-rem ----------------------------
-rem OpenFOAM environment laden
-rem ----------------------------
 set FOAM_INIT=/home/ofuser/OpenFOAM/OpenFOAM-v2212/etc/bashrc
 
-rem ----------------------------
-rem Alles nach dem ersten Argument = Command
-rem ----------------------------
 set CMD=%*
 
 if "%CMD%"=="" (
@@ -22,9 +16,6 @@ if "%CMD%"=="" (
     exit /b 1
 )
 
-rem ----------------------------
-rem Start MSYS2 + OpenFOAM + Command
-rem ----------------------------
 usr\bin\bash.exe -lc "source %FOAM_INIT% etc-mingw/pref.sh && %CMD%"
 
 endlocal
